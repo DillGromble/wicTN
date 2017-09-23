58ed79d42c2c6bd96e4de1a56fc470568346af07
Cory Harkins <CoryHarkins@Corys-MacBook-Pro.local>
2017-09-23 15:42:27 -0500

    built item component

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
 const Item = (props) => 
-    <li className="add-list-item">
-        
-    </li>
+    <div className="item-container">
+    	<div className="loc-graphic">
+    		<img src="input src here" alt="" />
+    	</div>
+    	<div className="text-body">
+    		<h1 className="loc-title">Location Title</h1>
+    		<p className="loc-details">Location details</p>
+    	</div>
+    </div>
 
 export default Item
