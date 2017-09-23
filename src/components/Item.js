e160f316ca5ef8325c3f9ffe0f5e6782c98fec62
Cory Harkins <CoryHarkins@Corys-MacBook-Pro.local>
2017-09-23 18:23:22 -0500

    added add menu options

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,15 +1,34 @@
 import React from 'react'
 import arrow from '../assets/arrow-icon.svg'
+import EbtIconLg from '../assets/ebt-icon-lg.svg'
+import SnapIconLg from '../assets/snap-icon-lg.svg'
+import WicIconLg from '../assets/wic-icon-lg.svg'
 
 const Item = (props) => 
-    <div className="item-container">
-    	<div className="loc-graphic">
-    		<img className="item-img" src={arrow} alt="" />
-    	</div>
-    	<div className="text-body">
-    		<h1 className="loc-title">Location Title</h1>
-    		<p className="loc-details">Location details</p>
-    	</div>
+	<div className="item-wrapper">
+	    <div className="item-container">
+	    	<div className="loc-graphic">
+	    		<img className="item-img" src={arrow} alt="" />
+	    	</div>
+	    	<div className="text-body">
+	    		<h1 className="loc-title">Location Title</h1>
+	    		<p className="loc-details">Location details</p>
+	    	</div>
+	    </div>
+	    <div className="add-item-container">
+	    	<h4>Click an icon to add that location to the map!</h4>
+			<div className="add-item-content">
+				<div className="add-icon add-ebt">
+					<img src={EbtIconLg} alt="add ebt location icon" />
+				</div>
+				<div className="add-icon add-snap">
+					<img src={SnapIconLg} alt="add snap location icon" />
+				</div>
+				<div className="add-icon add-wic">
+					<img src={WicIconLg} alt="add wic location icon" />
+				</div>
+			</div>
+	    </div>
     </div>
 
 export default Item
--- src/components/OptionsList.js
+++ src/components/OptionsList.js
@@ -6,41 +6,44 @@ import WicIconLg from '../assets/wic-icon-lg.svg'
 
 const OptionsList = (props) => {
 	return (
-    <ul className="icon-list">
-        <li className="icon ebt">
-			<div className="checkbox">
-				<input type="checkbox"  />
-			</div>
-        	<div className="icon-img">
-        		<img src={EbtIconLg} alt="ebt icon" />
-        	</div>
-        	<div className="icon-text">
-        		<h3>EBT</h3>
-        	</div>
-        </li>
-        <li className="icon snap">
-			<div className="checkbox">
-				<input type="checkbox"  />
-			</div>
-        	<div className="icon-img">
-        		<img src={SnapIconLg} alt="snap icon" />
-        	</div>
-        	<div className="icon-text">
-        		<h3>SNAP</h3>
-        	</div>
-        </li>
-        <li className="icon wic">
-			<div className="checkbox">
-				<input type="checkbox"  />
-			</div>
-        	<div className="icon-img">
-        		<img src={WicIconLg} alt="wic icon" />
-        	</div>
-        	<div className="icon-text">
-        		<h3>WIC</h3>
-        	</div>
-        </li>
-    </ul>
+	<div className="options-container">
+		<h3>Select an option below to filter results</h3>
+	    <ul className="icon-list">
+	        <li className="icon ebt">
+				<div className="checkbox">
+					<input type="checkbox"  />
+				</div>
+	        	<div className="icon-img">
+	        		<img src={EbtIconLg} alt="ebt icon" />
+	        	</div>
+	        	<div className="icon-text">
+	        		<h3>EBT</h3>
+	        	</div>
+	        </li>
+	        <li className="icon snap">
+				<div className="checkbox">
+					<input type="checkbox"  />
+				</div>
+	        	<div className="icon-img">
+	        		<img src={SnapIconLg} alt="snap icon" />
+	        	</div>
+	        	<div className="icon-text">
+	        		<h3>SNAP</h3>
+	        	</div>
+	        </li>
+	        <li className="icon wic">
+				<div className="checkbox">
+					<input type="checkbox"  />
+				</div>
+	        	<div className="icon-img">
+	        		<img src={WicIconLg} alt="wic icon" />
+	        	</div>
+	        	<div className="icon-text">
+	        		<h3>WIC</h3>
+	        	</div>
+	        </li>
+	    </ul>
+    </div>
 	)
 }
 
