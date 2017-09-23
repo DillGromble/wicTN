b091b5d5564981e64c9d94826b691484f43788c7
Gabriel <gabriel.wrowe@gmail.com>
2017-09-23 15:05:08 -0500

    feat: places working and showing names

--- src/map.jsx
+++ src/map.jsx
@@ -11,6 +11,8 @@ export default class GoogleMap extends React.Component {
       userLoc: null,
       service: null
     }
+
+    // this.state.service = this.state.service.bind(this)
   }
 
   componentWillMount() {
@@ -18,37 +20,25 @@ export default class GoogleMap extends React.Component {
       const coords = pos.coords
       const center = new google.maps.LatLng(coords.latitude, coords.longitude);
 
-
       // this will be tied to button.
       this.state.map.setZoom(15)
       this.state.map.setCenter(center) // TODO: move to button and pull center from state.userLoc
+
       const request = {
         location: center,
-        radius: '1155581.153000',
+        radius: '1128.497720',
         type: ['food']
-      };
+      }
 
       this.state.service.nearbySearch(request, (results, status) => {
         if (status == google.maps.places.PlacesServiceStatus.OK) {
           for (var i = 0; i < results.length; i++) {
-            var place = results[i];
-            createMarker(results[i]);
+            let place = results[i];
+            this.createMarker(place, place.name);
           }
         }
-      }
+      })
     }
-
-    var marker = new google.maps.Marker({
-      position: myLatlng,
-      title:"Hello World!"
-  });
-
-  // To add the marker to the map, call setMap();
-  marker.setMap(map);
-
-
-
-
 
     const error = err => {
       console.error(`ERROR(${err.code}): ${err.message}`);
@@ -66,6 +56,33 @@ export default class GoogleMap extends React.Component {
     const service = new google.maps.places.PlacesService(map)
     this.setState({ map, service })
   }
+
+  createMarker(place, title) {
+    const infoWindow = new google.maps.InfoWindow();
+
+    const marker = new google.maps.Marker({
+      position: place.geometry.location,
+      title
+    });
+
+    const getDetails = this.state.service.getDetails.bind(this.state.service)
+
+    google.maps.event.addListener(marker, 'click', () => {
+      getDetails(place, (result, status) => {
+        if (status !== google.maps.places.PlacesServiceStatus.OK) {
+          console.error(status);
+          return;
+        }
+        infoWindow.setContent(result.name);
+        infoWindow.open(this.state.map, marker);
+      });
+    });
+
+    console.log('testing marker', marker)
+    // To add the marker to the map, call setMap();
+    marker.setMap(this.state.map);
+  }
+
 
   render() {
     return (
